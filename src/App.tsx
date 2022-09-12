891c10f901b7b46a0c4762588d482c42e07f6879
Brian Bowman <seeker14491@gmail.com>
2022-09-12 06:02:46 -0500

    Add basic error handling and loading text

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { Component, createMemo, createSignal, For } from "solid-js";
+import {
+  Component,
+  createMemo,
+  createSignal,
+  For,
+  Match,
+  Switch,
+} from "solid-js";
 import { FaBrandsGithub } from "solid-icons/fa";
 
 type Entry = [string, number];
 
 const [fetchedEntries, setFetchedEntries] = createSignal<Entry[]>([]);
+const [fetchState, setFetchState] = createSignal<"loading" | "done" | "failed">(
+  "loading"
+);
 
 const ranks = createMemo(() => {
   const entries = fetchedEntries();
@@ -24,10 +34,16 @@ const ranks = createMemo(() => {
 });
 
 const update = async () => {
-  const response = await (
-    await fetch("https://neodash-hall-of-fame-api.seekr.pw/")
-  ).json();
-  setFetchedEntries(response);
+  try {
+    const response = await (
+      await fetch("https://neodash-hall-of-fame-api.seekr.pw/")
+    ).json();
+    setFetchedEntries(response);
+    setFetchState("done");
+  } catch (e) {
+    console.error("failed to fetch Hall of Fame data: " + e);
+    setFetchState("failed");
+  }
 };
 
 update();
@@ -37,28 +53,40 @@ const App: Component = () => {
   return (
     <div class="flex flex-col items-center text-white">
       <h1 class="m-4 text-center text-3xl font-bold">Neodash Hall of Fame</h1>
-      <div class="mx-auto max-w-full">
-        <table class="table-compact mx-1 my-2 table">
-          <thead>
-            <tr>
-              <th class="sticky top-0 text-xl">Rank</th>
-              <th class="sticky top-0 text-xl">Player</th>
-              <th class="sticky top-0 text-xl">Score</th>
-            </tr>
-          </thead>
-          <tbody>
-            <For each={fetchedEntries()}>
-              {(entry, i) => (
+      <Switch>
+        <Match when={fetchState() === "loading"}>
+          <p class="text-xl">Loading...</p>
+        </Match>
+        <Match when={fetchState() === "failed"}>
+          <p class="text-xl">Failed to load data.</p>
+        </Match>
+        <Match when={fetchState() === "done"}>
+          <div class="mx-auto max-w-full">
+            <table class="table-compact mx-1 my-2 table">
+              <thead>
                 <tr>
-                  <td class="text-xl font-bold">#{ranks()[i()]}</td>
-                  <td class="text-xl">{entry[0]}</td>
-                  <td class="text-right text-xl font-semibold">{entry[1]}</td>
+                  <th class="sticky top-0 text-xl">Rank</th>
+                  <th class="sticky top-0 text-xl">Player</th>
+                  <th class="sticky top-0 text-xl">Score</th>
                 </tr>
-              )}
-            </For>
-          </tbody>
-        </table>
-      </div>
+              </thead>
+              <tbody>
+                <For each={fetchedEntries()}>
+                  {(entry, i) => (
+                    <tr>
+                      <td class="text-xl font-bold">#{ranks()[i()]}</td>
+                      <td class="text-xl">{entry[0]}</td>
+                      <td class="text-right text-xl font-semibold">
+                        {entry[1]}
+                      </td>
+                    </tr>
+                  )}
+                </For>
+              </tbody>
+            </table>
+          </div>
+        </Match>
+      </Switch>
       <a
         class="link link-hover m-4 mt-8 flex items-center gap-1"
         href="https://github.com/Seeker14491/neodash-hall-of-fame"
